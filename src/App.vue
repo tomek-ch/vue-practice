582e4cb5978fbd12434be51c88c3d875b00446a7
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-17 22:49:50 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NewTaskForm @add="addTask" />
+  <TaskList
+    :tasks="tasks"
+    @delete="remove"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewTaskForm from './components/NewTaskForm.vue';
+import TaskList from './components/TaskList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NewTaskForm,
+    TaskList,
+  },
+  data() {
+    return {
+      tasks: JSON.parse(localStorage.getItem("tasks")) || [],
+    }
+  },
+  methods: {
+    addTask(text) {
+      this.tasks = [...this.tasks, {
+        id: Date.now(),
+        text,
+        done: false,
+      }];
+    },
+    remove(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+  },
+  watch: {
+    tasks: {
+      handler(tasks) {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+      },
+      deep: true,
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: auto;
+  padding: 2em;
+}
+
+button {
+  padding: 0.5em 1em;
+  border-radius: 5px;
+  border: none;
+  background-color: #ddd;
+  outline: none;
+  font-size: inherit;
+}
+
+button:hover {
+  background-color: #ccc;
+}
+
+input:hover {
+  background-color: #ccc;
+}
+
+input {
+  outline: none;
+  font-size: inherit;
+}
+
+input:focus-visible {
+  box-shadow: 0 0 0 2px dodgerblue;
+}
+
+button:focus-visible {
+  box-shadow: 0 0 0 2px dodgerblue;
 }
 </style>
---
+++ src/components/NewTaskForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit="handleSubmit" class="container">
+    <input v-model="input" placeholder="New task" />
+    <button :disabled="!input">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewTaskForm",
+  props: ["addTask"],
+  emits: ["add"],
+  data() {
+    return {
+      input: "",
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.$emit("add", this.input);
+      this.input = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  gap: 1em;
+}
+
+input {
+  flex-grow: 1;
+  padding: 0.5em;
+  border: none;
+  background-color: lightgray;
+  border-radius: 5px;
+  font-size: inherit;
+  outline: none;
+  flex-shrink: 1;
+  width: 0;
+}
+</style>
